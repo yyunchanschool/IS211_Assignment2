6a20c0375fdf6f2983116a946a35aa74064511d4
yyunchanschool <78572336+yyunchanschool@users.noreply.github.com>
2021-02-15 22:58:24 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -2,21 +2,38 @@ import argparse
 import urllib.request
 import logging
 import datetime
+import csv
 
 def downloadData(url):
     """Downloads the data"""
-    pass
+    url = 'https://s3.amazonaws.com/cuny-is211-spring2015/birthdays100.csv'
+    response = urllib.request.urlopen(url)
+    data = response.read().decode('utf-8')
+    return data
 
-def processData(file_content):
-    pass
+def processData(data):
+    csvData = csv.reader(data)
+    personData = {}
+    next(csvData)
 
+    for row in csvData:
+        try:
+            row(2) = datetime.strptime(row(2), '%d/%m/%y')
+        except ValueError:
+            id = int(row(0))
+            line = int(row(0))+1
+            logger = logging.getLogger('assignment2')
+            logger.error('Error processing line #'[line]' for ID #'[id]", format(line, id))
+    return personData
 
 def displayPerson(id, personData):
-    pass
+    try reply = 'Person #'[id]' is '[name]' with a birthday of '[date]'
+        print(reply)
+    except KeyError:
+        print('No user found')
 
 def main(url):
     print(f"Running main with URL = {url}...")
-
 
 if __name__ == "__main__":
     """Main entry point"""
@@ -24,3 +41,4 @@ if __name__ == "__main__":
     parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
     args = parser.parse_args()
     main(args.url)
+    logging.basicConfig(filename="errors.log", level=logging.ERROR)
